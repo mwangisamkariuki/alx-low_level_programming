1453f30bd9a9b4b1be12afe6f6cc3d24e57424bb
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-06-30 12:14:55 -0700

    Write a C program that prints exactly a statement followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "and that piece of art is useful"
+ * -Dora korpar,2015-10-19 "
+ * followed by a new line, to the new tandard error.
+ * Return: 1 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
