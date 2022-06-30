bbe7f13fd5470d79cebdd9d1d3a407b6d49a67f9
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-06-30 10:20:23 -0700

    a script that generates the assembly code of a C code and save it in an output file.

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main (void)
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
-printf ("Hello World of C!\n");
-return 0;
+	    return (0);
 }
