2d94de98a851e21ae882a978601ffde04629327b
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-12 01:21:44 +0300

    Print an array of size n

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[7];
+
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    array[5] = -198;
+    array[6] = 298;
+    array[7] = -1024;
+    print_array(array, 7);
+    return (0);
+}
