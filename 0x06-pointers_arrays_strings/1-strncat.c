77f2b0223bf4e342c0129036e1264172ea668a74
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-14 22:04:55 +0300

    convert all lowercase to uppercase

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,9 +6,12 @@
  * @src: variable that the string is to append
  * @n: the maximum number of bytes to append from src
  *
- * FUnction Description: This function appends the string pointed to by src to the
- * string pointed to by dest, overwriting the null byte at the end of dest,
- * and then adding a terminating null byte, using most n bytes from src.
+ * FUnction Description: This function appends
+ * the string pointed to by src to the
+ * string pointed to by dest,
+ * overwriting the null byte at the end of dest,
+ * and then adding a terminating null byte,
+ * using most n bytes from src.
  * Return: a pointer to the destination string dest
  */
 char *_strncat(char *dest, char *src, int n)
