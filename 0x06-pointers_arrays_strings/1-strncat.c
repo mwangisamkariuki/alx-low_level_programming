42b158cadbf3427b58ddc16d445a8914029f1e2f
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-14 21:26:55 +0300

    Write a function that copies a string.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - function concatenate two strings
+ * @dest: variable that the string is to append to
+ * @src: variable that the string is to append
+ * @n: the maximum number of bytes to append from src
+ *
+ * FUnction Description: This function appends the string pointed to by src to the
+ * string pointed to by dest, overwriting the null byte at the end of dest,
+ * and then adding a terminating null byte, using most n bytes from src.
+ * Return: a pointer to the destination string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *dest_end = dest;
+	int src_len = 0;
+
+	while (*dest_end)
+		++dest_end;
+
+	while (src_len < n && src[src_len])
+		++src_len;
+
+	if (src + src_len < dest || dest_end + src_len < src)
+	{
+		while (src_len--)
+			*dest_end++ = *src++;
+		*dest_end = '\0';
+	}
+
+	return (dest);
+}
