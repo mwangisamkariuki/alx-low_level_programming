6e5865d7964284014b9251974e90dcab7b61c8a4
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-19 14:51:28 +0300

    Write a function that copies memory area.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*Write a function that fills memory with a constant byte.*/
+/*Write a function that fills memory with a constant byte*/
 char *_memset(char *s, char b, unsigned int n);
 
-#endif /* MAIN_H */
+/*Write a function that copies memory area.*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
+
+#endif/* MAIN_H */
