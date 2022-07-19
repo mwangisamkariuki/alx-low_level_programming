80031bfb64e6727389ca89a4e3e1d5c0923efd98
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-19 15:43:16 +0300

    a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@ char *_memset(char *s, char b, unsigned int n);
 /*Write a function that copies memory area.*/
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/* Write a function that locates a character in a string */
+char *_strchr(char *s, char c);
 
 
 #endif/* MAIN_H */
