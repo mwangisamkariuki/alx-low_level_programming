265f76dac4e1b693fc72ce0a4e4044fda4ada4d9
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-19 15:58:25 +0300

    Write a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,7 @@ char *_strchr(char *s, char c);
 /*a function that gets the length of a prefix substring.*/
 unsigned int _strspn(char *s, char *accept);
 
+/*a function that searches a string for any of a set of bytes.*/
+char *_strpbrk(char *s, char *accept);
+
 #endif/* MAIN_H */
