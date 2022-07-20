75054ef11eeb003cd13b9e42a57a7804c54181db
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-20 17:57:08 +0300

    a function that prints a string in reverse.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,4 +4,6 @@
 /*a function that prints a string, followed by a new line.*/
 void _puts_recursion(char *s);
 
+/*a function that prints a string in reverse.*/
+void _print_rev_recursion(char *s);
 #endif
