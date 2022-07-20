bc989dc911d02a4606c5fe968d7d3ab73198eaa0
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-20 18:09:18 +0300

    a function that returns the length of a string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,8 @@ void _puts_recursion(char *s);
 
 /*a function that prints a string in reverse.*/
 void _print_rev_recursion(char *s);
+
+/*a function that returns the length of a string.*/
+int _strlen_recursion(char *s);
+
 #endif
