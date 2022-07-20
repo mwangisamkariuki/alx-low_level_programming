1766dfb70cd03a43a620a39341a835f430dcb40c
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-20 18:52:41 +0300

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -31,4 +31,8 @@ int is_prime_number(int n);
 /*Write a function that returns 1 if a string is a palindrome and 0 if not.*/
 int is_palindrome(char *s);
 
+/*a function that compares two strings and returns 1 if the strings can be
+considered identical, otherwise return 0.*/
+int wildcmp(char *s1, char *s2);
+
 #endif
