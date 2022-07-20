aad9029ee52320ebe110ba5588dc38a169bc20b1
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-20 18:27:37 +0300

    function that returns the value of x raised to the power of y

--- 0x08-recursion/4-main.c
+++ 0x08-recursion/4-main.c
@@ -1,20 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
- * _pow_recursion - Performs power of a number
- * @x: Integer Value
- * @y: Power Value
- * Return: Factorial
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int _pow_recursion(int x, int y)
+int main(void)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-	else
-	{
-		if (y > 0)
-			return (x * (_pow_recursion(x, y - 1)));
-		else
-			return (1);
-	}
+    int r;
+
+    r = _pow_recursion(1, 10);
+    printf("%d\n", r);
+    r = _pow_recursion(1024, 0);
+    printf("%d\n", r);
+    r = _pow_recursion(2, 16);
+    printf("%d\n", r);
+    r = _pow_recursion(5, 2);
+    printf("%d\n", r);
+    r = _pow_recursion(5, -2);
+    printf("%d\n", r);
+    r = _pow_recursion(-5, 3);
+    printf("%d\n", r);
+    return (0);
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,20 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _pow_recursion - Performs power of a number
+ * @x: Integer Value
+ * @y: Power Value
+ * Return: Factorial
  */
-int main(void)
+int _pow_recursion(int x, int y)
 {
-    int r;
-
-    r = _pow_recursion(1, 10);
-    printf("%d\n", r);
-    r = _pow_recursion(1024, 0);
-    printf("%d\n", r);
-    r = _pow_recursion(2, 16);
-    printf("%d\n", r);
-    r = _pow_recursion(5, 2);
-    printf("%d\n", r);
-    r = _pow_recursion(5, -2);
-    printf("%d\n", r);
-    r = _pow_recursion(-5, 3);
-    printf("%d\n", r);
-    return (0);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (y > 0)
+			return (x * (_pow_recursion(x, y - 1)));
+		else
+			return (1);
+	}
 }
