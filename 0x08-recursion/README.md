ee3e8b41cca22bdf4dc924d7de35088252cecf8d
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-20 17:48:06 +0300

    a function that prints a string, followed by a new line

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+# Task 0;
+a function that prints a string, followed by a new line.
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*a function that prints a string, followed by a new line.*/
+void _puts_recursion(char *s);
+
+#endif
