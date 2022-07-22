9e649f46abf38e43f24311f7618a15f6a734dccc
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-07-22 17:27:47 +0300

    Create the static library libmy.a containing all the functions in main.h

---
+++ 0x09-static_libraries/7strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - function culculates the length of a given string
+ * @s: string where to check the length
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+}
