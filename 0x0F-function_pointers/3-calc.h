3bbb59d6176b7eb9a16ae9309a4fa4633ba291f3
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-08-04 17:44:07 +0300

    Betty linter correction on calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -10,11 +10,11 @@
  *
  * @op: The operator
  * @f: The function associated
- */
+*/
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 
 
