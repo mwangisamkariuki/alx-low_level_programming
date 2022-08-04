1c1f8629b504470099a66f2f9a088c7372bbab9a
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-08-04 21:29:07 +0300

    a function that prints strings, followed by a new line.

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django \n");
+    print_strings(", ", 2, "samuel", "mwangi kariuki \n");
+    return (0);
+}
