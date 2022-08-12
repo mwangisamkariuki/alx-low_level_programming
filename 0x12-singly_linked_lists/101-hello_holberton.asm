01c529e14130c6ceda32361369cb449e4014a5e3
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-08-12 13:31:10 +0300

    Write a 64-bit program in assembly that prints Hello, Holberton\0

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,9 @@
-#include <stdio.h>
-
-int main() {
-    // Write C code here
-    printf("Hello\nHerborton");
-
-    return 0;
-}
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
